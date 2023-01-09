c2ff5d9fd6508cc15a8cfcd712bed9691647bf7f
Patrick Bomar <patrick.bomar@datastax.com>
2023-01-09 15:42:00 -0600

    Update TestCase4 script and Readme

--- starlight-for-jms/testcases/raw_definition/testcase4_producers/pre_task.sh
+++ starlight-for-jms/testcases/raw_definition/testcase4_producers/pre_task.sh
@@ -2,6 +2,6 @@
 # Note - Define PATH to localhost Pulsar software binaries
 ## Nothing to do for pre_task in this testcase4, just start the producers
 export PATH=~/current/bin:$PATH
-#pulsar-shell -c ~/newproject/nbs4j_test_jms_framework/pulsar_conn/client.conf \
-#   -f ~/newproject/nbs4j_test_jms_framework/testcases/raw_definition/testcase4_producers/create_topics.txt -np
+#pulsar-shell -c $PWD/pulsar_conn/client.conf \
+#   -f $PWD/testcases/raw_definition/testcase4_producers/create_topics.txt -np
 sleep 2
